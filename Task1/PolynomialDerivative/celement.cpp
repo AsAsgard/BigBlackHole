ce2bc39f91c66ff35bd159c0b31052d44b9de37a
AsAsgard <dbrylkin96@gmail.com>
2018-10-01 14:22:35 +0300

    Add class celement

---
+++ Task1/PolynomialDerivative/celement.cpp
@@ -0,0 +1,6 @@
+#include "celement.h"
+
+cElement::cElement()
+{
+
+}
---
+++ Task1/PolynomialDerivative/celement.h
@@ -0,0 +1,11 @@
+#ifndef CELEMENT_H
+#define CELEMENT_H
+
+
+class cElement
+{
+public:
+    cElement();
+};
+
+#endif // CELEMENT_H
