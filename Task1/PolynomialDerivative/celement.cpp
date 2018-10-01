64407ffc3548ea6042864b58e4a3211b702587c7
AsAsgard <dbrylkin96@gmail.com>
2018-10-01 16:38:18 +0300

    Commented celement.cpp

--- Task1/PolynomialDerivative/celement.cpp
+++ Task1/PolynomialDerivative/celement.cpp
@@ -1,12 +1,16 @@
 #include "celement.h"
 
+/*
+ *  Getting derivative from one element of poly
+ * */
+
 void cElement::Derivation(void)
 {
     if (Degree > 0)
     {
-        Coef *= Degree;
+        Coef *= Degree;     // d(c*x^a)/dx = (c*a)*x^(a-1)
         Degree--;
     } else {
-        Coef = 0;
+        Coef = 0;   // if degree == 0, then derivative = 0 too.
     }
 }
